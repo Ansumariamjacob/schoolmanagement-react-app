1990e7f4c910153f89964adbd42fcb2b58824ad3
ansumariamjacob <ansumariam99@gmail.com>
2022-05-25 22:35:16 +0530

    school managementaddfaculity

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Addstudent from './component/Addstudent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Search from './component/Search';
+import Addfaculity from './component/Addfaculity';
 
 function App() {
   return (
     <div className="App">
       <Addstudent/>
       <Search/>
+      <Addfaculity/>
     </div>
   );
 }
