cfdb636470823d76036c2be40e65bafc27068eaa
ansumariamjacob <ansumariam99@gmail.com>
2022-05-25 23:36:04 +0530

    viewfac

---
+++ src/component/Viewfac.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const Viewfac = () => {
+  return (
+    <div>
+        <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        <table className="table table-striped">
+  <thead>
+    <tr>
+      
+      
+      <th scope="col">Name</th>
+      <th scope="col">Education</th>
+      <th scope="col">Mobile number</th>
+      <th scope="col">Address</th>
+      <th scope="col">Pincode</th>
+      <th scope="col">District</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Priya</th>
+      <td>Mtech</td>
+      <td>9801234111</td>
+      <td>krishor nivas</td>
+      <td>690104</td>
+      <td>pathanamthitta</td>
+      
+    </tr>
+    <tr>
+    <th scope="row">Ravi</th>
+      <td>M.Ed</td>
+      <td>7865090999</td>
+      <td>puthanvittu</td>
+      <td>690107</td>
+      <td>Thissur</td>
+      
+    </tr>
+    <tr>
+    <th scope="row">Reena</th>
+      <td>Msc</td>
+      <td>8979798561</td>
+      <td>malayali vittu</td>
+      <td>690114</td>
+      <td>Kollam</td>
+      
+    </tr>
+  </tbody>
+</table>
+        </div>
+    </div>
+</div>
+    </div>
+  )
+}
+
+export default Viewfac
